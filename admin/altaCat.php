ccd1ad6fe63b98e5fb2f2136ad0ee220eeaa7ff5
alanlopez0208 <alan666_alan@hotmail.com>
2023-04-15 14:27:12 -0600

    Ediciones del 1 de Abril

---
+++ admin/altaCat.php
@@ -0,0 +1,21 @@
+<?php
+include("../conexion.php");
+
+$categoria = $_POST["cate"];
+$catPadre = $_POST["categoriaSup"];
+$img = "img/catalogo/categorias/" . $_POST["imagen"];
+$desc = $_POST["descripcion"];
+
+$stm = $conn->prepare("INSERT INTO `categorias`
+    (`categoria`, `img`,`categoriaPadre`,`descripcion`)
+VALUES (?,?,?,?)");
+$stm->bindParam(1, $categoria);
+$stm->bindParam(2, $img);
+$stm->bindParam(3, $catPadre);
+$stm->bindParam(4, $desc);
+
+if ($stm->execute()) {
+    header("Location:categorias.php");
+}
+
+?>
