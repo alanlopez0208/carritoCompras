564bd3ff73996569f00bc8d36d5007127ce91574
alanlopez0208 <alan666_alan@hotmail.com>
2023-05-02 00:23:20 -0600

    Hola Mundo

--- admin/editCat.php
+++ admin/editCat.php
@@ -4,30 +4,31 @@ include("../conexion.php");
 $id = $_POST["idEdit"];
 $categoria = $_POST["catEdit"];
 $catPadre = $_POST["catPadreEdit"];
-$img ="";
+$img = "";
 
-if(empty($_FILES['img']['name']) ){
+if (empty($_FILES['img']['name'])) {
     $img = $_POST["imgNoChange"];
-}else{
-    $img = "img/categorias/".$_FILES['img']['name'];
+} else {
+    $img = "img/categorias/" . $_FILES['img']['name'];
 }
 
 $desc = $_POST["descEdit"];
-try{
+try {
     $stm = $conn->prepare("UPDATE `categorias` SET `categoria`=?, `img`=?, `categoriaPadre`=?, `descripcion`=? WHERE `id`=?");
     $stm->bindParam(1, $categoria);
     $stm->bindParam(2, $img);
     $stm->bindParam(3, $catPadre);
     $stm->bindParam(4, $desc);
     $stm->bindParam(5, $id);
-    
+
     if ($stm->execute()) {
         include("subirImg.php");
         header("Location:categorias.php");
     }
-}catch(PDOException $e){
-    echo "Error: ". $e->getMessage();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
 
 $conn = null;
+echo 'Hola Mundo';
 ?>
